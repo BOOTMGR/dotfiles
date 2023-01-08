e22d8e9b6474304914852569029f344e187e7ec4
Harsh Panchal <BOOTMGR@users.noreply.github.com>
2023-01-08 10:41:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,18 @@
 # export kitty terminfo
 sudo ln -s /home/user/.local/kitty.app/share/terminfo/x/xterm-kitty /usr/share/terminfo/x/xterm-kitty
 
+# disable visual mode in vim
+echo "set mouse-=a" >> ~/.vimrc
+echo "set ttymouse=" >> ~/.vimrc
+echo "source \$VIMRUNTIME/defaults.vim" >> ~/.vimrc
+
+# setup env
+mkdir -p /home/$USER/.local/bin
+echo 'export PATH=$PATH:/home/$USER/.local/bin' >> .bashrc
+echo 'alias ll="ls -lrth"' >> .bashrc
+
+# Downoad font
+mkdir -p ~/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip -O /tmp/font.zip
+unzip -j /tmp/font.zip "agave regular Nerd Font Complete Mono.ttf" -d ~/.fonts/
+fc-cache -vf
